547c49b70cdfcff308131453999aab550c4ba109
Joydeip <joydippaul2004@gmail.com>
2025-09-11 18:16:39 +0530

    add 3 arrays code

---
+++ dsa-leetcode-solutions/Arrays/best_time_buy_and_sell_stock.java
@@ -0,0 +1,15 @@
+// leetcode 121, Best Time to Buy and Sell Stock 
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min_price = prices[0];
+        int maxprof = 0;
+
+        for(int i=1;i<prices.length;i++){
+            min_price = Math.min(prices[i],min_price);
+            maxprof = Math.max(maxprof,prices[i] - min_price);
+        }
+
+        return maxprof;
+    }
+}
