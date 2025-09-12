745119f356809e188c2486fa0e0ed8570d2707c7
Joydeip <joydippaul2004@gmail.com>
2025-09-12 22:10:33 +0530

    Create jump_game.java

---
+++ dsa-leetcode-solutions/Arrays/jump_game.java
@@ -0,0 +1,13 @@
+// leetcode 55, Jump Game
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxreach=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>maxreach) return false;
+            maxreach= Math.max(maxreach,i+nums[i]);
+        }
+        return true;
+    }
+}
