1d3a5d58f78247c7fc9ca4f62ee961c8fbcc8373
Joydeip <joydippaul2004@gmail.com>
2025-09-17 23:28:44 +0530

    Update zigzag_conversion.java

--- dsa-leetcode-solutions/Strings/zigzag_conversion.java
+++ dsa-leetcode-solutions/Strings/zigzag_conversion.java
@@ -1,1 +1,21 @@
-// leetcode 
+// leetcode 6, Zigzag Conversion
+
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+        if(numRows == 1)
+            return s;
+        for(int i = 0; i < numRows; i++) 
+        {
+            for(int j = i; j < s.length(); j += ((numRows - 1)*2)) 
+            {
+                sb.append(s.charAt(j));
+                if (i > 0 && i < numRows - 1 && (j + ((numRows - 1)*2) - (i*2)) < s.length()) 
+                {
+                    sb.append(s.charAt(j + ((numRows - 1)*2) - (i*2)));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
